58de54d59949e01564794642a3d8833b5263898b
Olemish <92503753+Olemish@users.noreply.github.com>
2022-06-18 22:01:40 +0000

    Deploying to gh-pages from @ Olemish/goit-js-hw-09@e205ac80f6651df4488605f16e5fd865c54b1b57 🚀

---
+++ 01-color-switcher.64727907.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let d=null;t.addEventListener("click",(()=>(d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,void(e.disabled=!1)))),e.addEventListener("click",(()=>(clearInterval(d),t.disabled=!1,void(e.disabled=!0))));
+//# sourceMappingURL=01-color-switcher.64727907.js.map
---
+++ 01-color-switcher.dbb4c4ed.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){return n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,void(e.disabled=!1)})),e.addEventListener("click",(function(){return clearInterval(n),t.disabled=!1,void(e.disabled=!0)}))}();
+//# sourceMappingURL=01-color-switcher.dbb4c4ed.js.map
