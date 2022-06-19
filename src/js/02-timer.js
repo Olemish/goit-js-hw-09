30e89870869bc3c1421318a49d9e5127d4717900
Olemish <olesia.hlushenkova@gmail.com>
2022-06-19 23:01:55 +0200

    task 2-3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,12 +2,15 @@
 import flatpickr from "flatpickr";
 // Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const daysEl = document.querySelector('span[data-days]');
 const hoursEl = document.querySelector('span[data-hours]');
 const minutesEl = document.querySelector('span[data-minutes]');
 const secondsEl = document.querySelector('span[data-seconds]');
 const buttonStart = document.querySelector('button[data-start]')
+buttonStart.disabled = true;
 
 let counter = 0;
 const options = {
@@ -16,25 +19,34 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-      counter = Date.parse(selectedDates[0]) - Date.parse(new Date())
-      
+      if (Date.parse(selectedDates)<Date.parse(new Date())) {
+          Notify.failure('Please choose a date in the future');
+      } 
+      else {
+       counter = Date.parse(selectedDates[0]) - Date.parse(new Date())
+       buttonStart.disabled = false;}
   },
 };
-
-
-
 
 
 buttonStart.addEventListener('click', () => handleButtonStart(counter))
 
 function handleButtonStart() {
-    const { days, hours, minutes, seconds } = convertMs(counter)
-      daysEl.textContent = days
-      hoursEl.textContent = hours
-      minutesEl.textContent = minutes
-      secondsEl.textContent = seconds
     
+    const timer = setInterval(() => {
+        // console.log(counter=counter - 1000)
+        const { days, hours, minutes, seconds } = convertMs(counter = counter - 1000)
+        const daysStr = days.toString()
+        const hoursStr = hours.toString()
+        const minutesStr = minutes.toString()
+        const secondsStr = seconds.toString()
+      daysEl.textContent = daysStr.length > 1 ? daysStr : addLeadingZero(daysStr)
+      hoursEl.textContent = hoursStr.length > 1 ? hoursStr : addLeadingZero(hoursStr)
+      minutesEl.textContent = minutesStr.length > 1 ? minutesStr : addLeadingZero(minutesStr)
+      secondsEl.textContent = secondsStr.length > 1 ? secondsStr : addLeadingZero(secondsStr)
+    }, 1000);
 }
+
 
 flatpickr("#datetime-picker", options);
 
@@ -44,7 +56,7 @@ function convertMs(ms) {
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
+    
   // Remaining days
   const days = Math.floor(ms / day);
   // Remaining hours
@@ -55,4 +67,7 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value){
+    return value.padStart(2, "0")
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,24 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+const promise = new Promise((resolve, reject) => {
+  // Asynchronous operation
+  setTimeout(() => {
+     const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      resolve({ position: position, delay:delay});
+    } else {
+      reject({ position: position, delay:delay});
+    }
+  }, delay);
+});
+  return promise
 }
+
+createPromise(2, 1500).then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
